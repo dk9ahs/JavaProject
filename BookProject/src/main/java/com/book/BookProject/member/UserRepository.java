0e8496cfdd906019c044022ea39f11554c73c758
HOON\hhhh <tle632@gmail.com>
2024-09-27 15:44:23 +0900

    클래스파일생성

---
+++ BookProject/src/main/java/com/book/BookProject/member/UserRepository.java
@@ -0,0 +1,10 @@
+package com.book.BookProject.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email);
+    UserEntity findByNick(String nick);
+}
