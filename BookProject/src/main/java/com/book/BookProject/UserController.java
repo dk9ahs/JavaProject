57db253cfae88fb88982d0f5942966d5f254f151
HOON\hhhh <tle632@gmail.com>
2024-09-27 15:52:10 +0900

    멤버관련 주석처리

--- BookProject/src/main/java/com/book/BookProject/UserController.java
+++ BookProject/src/main/java/com/book/BookProject/UserController.java
@@ -1,41 +1,41 @@
-package com.book.BookProject;
-
-import com.book.BookProject.member.UserEntity;
-import com.book.BookProject.member.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/user")
-public class UserController {
-
-    private final UserService userService;
-
-    // 회원 가입
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody UserEntity userEntity) {
-        if (userService.checkEmailDuplication(userEntity.getEmail())) {
-            return ResponseEntity.badRequest().body("이메일이 이미 존재합니다.");
-        }
-        if (userService.checkNickDuplication(userEntity.getNick())) {
-            return ResponseEntity.badRequest().body("닉네임이 이미 존재합니다.");
-        }
-
-        userService.registerUser(userEntity);
-        return ResponseEntity.ok("회원가입 성공!");
-    }
-
-    // 닉네임 중복 확인
-    @GetMapping("/checkNick")
-    public ResponseEntity<Boolean> checkNickDuplication(@RequestParam String nick) {
-        return ResponseEntity.ok(userService.checkNickDuplication(nick));
-    }
-
-    // 이메일 중복 확인
-    @GetMapping("/checkEmail")
-    public ResponseEntity<Boolean> checkEmailDuplication(@RequestParam String email) {
-        return ResponseEntity.ok(userService.checkEmailDuplication(email));
-    }
-}
+//package com.book.BookProject;
+//
+//import com.book.BookProject.member.UserEntity;
+//import com.book.BookProject.member.UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/user")
+//public class UserController {
+//
+//    private final UserService userService;
+//
+//    // 회원 가입
+//    @PostMapping("/register")
+//    public ResponseEntity<String> registerUser(@RequestBody UserEntity userEntity) {
+//        if (userService.checkEmailDuplication(userEntity.getEmail())) {
+//            return ResponseEntity.badRequest().body("이메일이 이미 존재합니다.");
+//        }
+//        if (userService.checkNickDuplication(userEntity.getNick())) {
+//            return ResponseEntity.badRequest().body("닉네임이 이미 존재합니다.");
+//        }
+//
+//        userService.registerUser(userEntity);
+//        return ResponseEntity.ok("회원가입 성공!");
+//    }
+//
+//    // 닉네임 중복 확인
+//    @GetMapping("/checkNick")
+//    public ResponseEntity<Boolean> checkNickDuplication(@RequestParam String nick) {
+//        return ResponseEntity.ok(userService.checkNickDuplication(nick));
+//    }
+//
+//    // 이메일 중복 확인
+//    @GetMapping("/checkEmail")
+//    public ResponseEntity<Boolean> checkEmailDuplication(@RequestParam String email) {
+//        return ResponseEntity.ok(userService.checkEmailDuplication(email));
+//    }
+//}
--- BookProject/src/main/java/com/book/BookProject/member/UserService.java
+++ BookProject/src/main/java/com/book/BookProject/member/UserService.java
@@ -1,29 +1,29 @@
-package com.book.BookProject.member;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@RequiredArgsConstructor
-public class UserService {
-
-    private final UserRepository userRepository;
-
-    // 회원 가입
-    @Transactional
-    public UserEntity registerUser(UserEntity userEntity) {
-        // 중복 검사 등 추가 로직
-        return userRepository.save(userEntity);
-    }
-
-    // 닉네임 중복 검사
-    public boolean checkNickDuplication(String nick) {
-        return userRepository.findByNick(nick) != null;
-    }
-
-    // 이메일 중복 검사
-    public boolean checkEmailDuplication(String email) {
-        return userRepository.findByEmail(email) != null;
-    }
-}
+//package com.book.BookProject.member;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class UserService {
+//
+//    private final UserRepository userRepository;
+//
+//    // 회원 가입
+//    @Transactional
+//    public UserEntity registerUser(UserEntity userEntity) {
+//        // 중복 검사 등 추가 로직
+//        return userRepository.save(userEntity);
+//    }
+//
+//    // 닉네임 중복 검사
+//    public boolean checkNickDuplication(String nick) {
+//        return userRepository.findByNick(nick) != null;
+//    }
+//
+//    // 이메일 중복 검사
+//    public boolean checkEmailDuplication(String email) {
+//        return userRepository.findByEmail(email) != null;
+//    }
+//}
