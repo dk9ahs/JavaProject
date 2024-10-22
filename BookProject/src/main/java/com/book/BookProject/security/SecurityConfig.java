a3a21e4b4046b29117bc02cc9237916ebb673d0b
HOON\hhhh <tle632@gmail.com>
2024-10-22 17:09:02 +0900

    병합전 커밋

--- BookProject/src/main/java/com/book/BookProject/security/SecurityConfig.java
+++ BookProject/src/main/java/com/book/BookProject/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                         .requestMatchers("/bestseller", "/bookdetail/**", "/mypage/**").permitAll()
                         .requestMatchers("/salesboard/**", "/inquiryboard/**").permitAll()
                         .requestMatchers("/order/**").authenticated()
-                        .requestMatchers("/", "/register", "/signup", "/login", "/findId", "/findPassword", "/IdCheck", "/NickCheck").permitAll()  // 추가
+                        .requestMatchers("/","/api/sms/send", "/register", "/signup", "/login", "/findId", "/findPassword", "/IdCheck", "/NickCheck").permitAll()  // 추가
                         .requestMatchers("/test").permitAll()
                         .requestMatchers("/guest/SocialSignup").permitAll()
                         .requestMatchers("/oauth2/**").permitAll()
